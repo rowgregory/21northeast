8d9dd7c45b9a519dc409cffdc05c7043e7dce682
rowgregory <rowgregory@gmail.com>
2024-10-28 12:25:41 -0400

    feat(fb-rich-preview):Add fb app id and metadata to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,41 @@
-import type { Metadata } from "next";
-import "./globals.css";
-import "./animations.css";
-import PageWrapper from "./page-wrapper";
+import type { Metadata } from 'next'
+import './globals.css'
+import './animations.css'
+import PageWrapper from './page-wrapper'
 
 export const metadata: Metadata = {
-  title: "Eileen Jonah",
-  description:
-    "This project enables users to easily search for and access residential real estate listings.",
-};
+  title: 'Eileen Jonah',
+  description: `Explore Century 21 North East Jonah Group's Next.js app powered by IDX Broker, delivering real-time property listings and market insights. Discover your ideal home with intuitive search, dynamic listings, and a seamless user experience.`,
+  metadataBase: new URL('https://jonahgroupre.com/'),
+  openGraph: {
+    type: 'website',
+    url: 'https://jonahgroupre.com/',
+    title: 'Eileen Jonah - Realtor ®',
+    description: 'Massachusetts Realtor',
+    images: [
+      {
+        url: 'https://firebasestorage.googleapis.com/v0/b/devon-hunt-nextjs.appspot.com/o/images%2Feileen-rp-4.png?alt=media&token=a9b5b5e8-1a69-4baa-8a86-49addbe1f973',
+        width: 1200,
+        height: 630,
+        alt: 'Eileen Jonah - Realtor ®'
+      }
+    ]
+  }
+}
 
 export default function RootLayout({
-  children,
+  children
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta property="fb:app_id" content="857941673220898" />
+      </head>
       <body>
         <PageWrapper>{children}</PageWrapper>
       </body>
     </html>
-  );
+  )
 }
