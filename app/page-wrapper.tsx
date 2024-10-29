6275b7dfceb5fec0535d9e9c9523016f2887d78b
rowgregory <rowgregory@gmail.com>
2024-10-29 15:10:56 -0400

    feat(hotjoar):Add hotjar

--- app/page-wrapper.tsx
+++ app/page-wrapper.tsx
@@ -1,13 +1,21 @@
 'use client'
 
-import React, { ReactNode, Suspense } from 'react'
+import React, { ReactNode, Suspense, useEffect } from 'react'
 import { ChakraProvider } from '@chakra-ui/react'
 import { Provider } from 'react-redux'
+import Hotjar from '@hotjar/browser'
 import { store } from './redux/store'
 import PageSlideWrapper from './page-slide-wrapper'
 import Loading from './loading'
 
+const siteId = 5189124
+const hotjarVersion = 6
+
 const PageWrapper = ({ children }: { children: ReactNode }) => {
+  useEffect(() => {
+    Hotjar.init(siteId, hotjarVersion)
+  }, [])
+
   return (
     <Suspense fallback={<Loading />}>
       <Provider store={store}>
